49f743d6eb5f7ad3cfdfe5a31cdd24663b433c79
iequeve <ioquellofalsos@gmail.com>
2026-01-16 10:20:04 +0100

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,4 @@
+// script.js
+// Optional JavaScript for additional interactivity if needed
+// For now, it's empty, but you can add things like smooth scrolling or more animations here.
+console.log('Website loaded successfully.');
